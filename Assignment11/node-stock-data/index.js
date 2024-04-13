a669d82b221aa2ebe7063104bb7965fecc11032a
shirisha reddy <122144371+shirishareddy28@users.noreply.github.com>
2024-04-13 06:22:41 -0400

    Add files via upload

---
+++ Assignment11/node-stock-data/index.js
@@ -0,0 +1,6 @@
+module.exports = {
+  exchanges: require('./functions/exchanges'),
+  stocks: require('./functions/stocks'),
+  stocksIntraday: require('./functions/stocksIntraday'),
+  tickers: require('./functions/tickers')
+}
---
+++ Assignment11/node-stock-data/util/formatOptionsExchanges.js
@@ -0,0 +1,39 @@
+const moment = require('moment');
+
+const VISIT = 'Visit https://marketstack.com/documentation for more information';
+
+function checkCorrectOptions(options) {
+  const {
+    limit, search, offset
+  } = options;
+  if (limit !== undefined && (Number(limit) < 1 || Number(limit) > 100)) {
+    throw new Error(`limit value should be between 1-100 ${VISIT}`);
+  }
+  // if(search == undefined){
+  //   throw new Error(`no search was specified. ${VISIT}`)
+  // }
+  if (offset !== undefined && (Number(offset) < 1 || Number(offset) > 100)) {
+    throw new Error(`offset value should be between 1-100 ${VISIT}`);
+  }
+}
+
+function formatOptions(options) {
+  let query = '&';
+  if (options === undefined) {
+    throw new Error(`Please specify options for the query. ${VISIT}`)
+  }
+  try {
+    checkCorrectOptions(options);
+  } catch (error) {
+    throw error;
+  }
+  const {
+    limit, search, offset
+  } = options;
+  query += limit !== undefined ? `limit=${limit}&` : '';
+  query += search !== undefined ? `search=${search}&` : '';
+  query += offset !== undefined ? `offset=${offset}&` : '';
+  return query;
+}
+
+module.exports = formatOptions;
